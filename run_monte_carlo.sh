35cf2a2fcaa5c609c90280f95d40183979062201
boris <bor1996@mail.ru>
2017-10-08 02:34:17 +0300

    init

---
+++ run_monte_carlo.sh
@@ -0,0 +1,44 @@
+# !/usr/bin/sh
+
+A=0
+B=1
+MAX=0
+MIN=1
+RUNS=1000
+TRUE_RES=0.33333
+
+N_WORKERS=1
+
+
+function lead_zero {
+	echo $1 | sed -e 's/^\./0./' -e 's/^-\./-0./'
+}
+
+DIFF=`bc <<< "scale=10; ($B - $A)/$N_WORKERS"`
+
+touch .tmp
+truncate -s 0 .tmp
+
+for i in `seq $N_WORKERS`; do
+	L=`bc <<< "$A + ($i-1) * $DIFF"`
+	U=`bc <<< "$A + $i * $DIFF"`
+	echo -e "$L $U $MAX $MIN $RUNS" >> .tmp
+done
+
+if [ -z "$1" ] 
+	then
+		RES=`parallel --no-notice -a .tmp --colsep ' ' ./main {}`
+	else
+		scp main ubuntu@ec2-34-207-99-68.compute-1.amazonaws.com:~/
+		RES=`parallel --no-notice -S $1 -a .tmp --colsep ' ' ./main {}`
+fi
+
+RES=`echo $RES | tr ' ' '\n' | awk '{s+=$1} END {print s}'`
+RES=`bc <<< "scale=10; $RES/$N_WORKERS"`
+RES=`lead_zero $RES`
+echo "Result      :" $RES 
+echo "True result :" $TRUE_RES 
+ERR=$(lead_zero `bc <<< "$RES-$TRUE_RES" | sed "s/^-//"`)
+echo "Error       :" $ERR
+
+rm .tmp
